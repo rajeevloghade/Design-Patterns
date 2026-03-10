a420cad0254c2e1535b804bfc4e2d5a2e27c7f25
rloghade-boku <rajeev.loghade@boku.com>
2026-03-10 20:57:40 +0530

    Builder Design Pattern

--- Builder Design Pattern/Main.java
+++ Builder Design Pattern/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        User user = new User.UserBuilder()
+                .setId("1")
+                .setName("John Doe")
+                .setEmail("johndoe@gmail.com").build();
+        System.out.println(user);
+
+        User jerry = User.UserBuilder.builder()
+                .setId("2")
+                .setName("Jerry")
+                .setEmail("jerry@gmail.com").build();
+        System.out.println(jerry);
+    }
 }
--- Builder Design Pattern/User.java
+++ Builder Design Pattern/User.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class User {
+public class User {
+
+    public String id;
+    public String name;
+    public String email;
+
+    private User(UserBuilder userBuilder) {
+        this.id = userBuilder.id;
+        this.name = userBuilder.name;
+        this.email = userBuilder.email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    static class UserBuilder {
+        public String id;
+        public String name;
+        public String email;
+
+        public static UserBuilder builder() {
+            return new UserBuilder();
+        }
+
+        public UserBuilder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build() {
+            User user = new User(this);
+            return user;
+        }
+    }
 }
